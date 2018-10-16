a2d65a495959ad91c9fb407770a16999c619287d
Michael Pope <popemray1986@gmail.com>
2018-10-16 18:24:51 +0000

    automatically backed up by learn

--- app/workers/songs_worker.rb
+++ app/workers/songs_worker.rb
@@ -1,8 +1,8 @@
 include Sidekiq::Worker
 
 def perform(songs_file)
-  CSV.foreach(leads_file, headers: true) do |lead|
-    Customer.create(email: lead[0], first_name: lead[1], last_name: lead[2])
+  CSV.foreach(songs_file, headers: true) do |song|
+    Song.create(title: song[0], artist_name: song[1])
   end
-  redirect_to customers_path
+  redirect_to songs_path
 end
